984fe5c3239ef8f6512629d5f2e7acad96e96f4d
ljh37694 <ljh37694@naver.com>
2024-01-24 20:53:32 +0900

    dataset을 이용해 이벤트 리스너 실습

--- JavascriptBasic/tab.js
+++ JavascriptBasic/tab.js
@@ -1,11 +1,22 @@
-for (let i = 0; i < $(".tab-button").length; i++) {
-    let curBtn = $(".tab-button").eq(i), curContent = $(".tab-content").eq(i);
-    console.log("hi");
+// for (let i = 0; i < $(".tab-button").length; i++) {
+//     let curBtn = $(".tab-button").eq(i), curContent = $(".tab-content").eq(i);
+//     console.log("hi");
 
-    curBtn.on("click", () => {
-        $(".tab-button").removeClass("orange");
-        $(".tab-content").removeClass("show");
-        curBtn.addClass("orange");
-        curContent.addClass("show");
+//     curBtn.on("click", () => {
+//         $(".tab-button").removeClass("orange");
+//         $(".tab-content").removeClass("show");
+//         curBtn.addClass("orange");
+//         curContent.addClass("show");
+//     });
+// }
+
+    $(".list").on("click", (e) => {
+        let id = Number(e.target.dataset.id);
+
+        if (e.target.classList.contains("tab-button")) {
+            $(".tab-button").removeClass("orange");
+            $(".tab-content").removeClass("show");
+            $(".tab-button").eq(id).addClass("orange");
+            $(".tab-content").eq(id).addClass("show");
+        }
     });
-}
