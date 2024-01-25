5697e8543ffc2b5c8228f342511b41cb3b36d8a7
ljh37694 <ljh37694@naver.com>
2024-01-25 19:00:08 +0900

    get으로 데이터를 받아와 더보기 누르면 상품 카드 추가하기

--- JavascriptBasic/list.js
+++ JavascriptBasic/list.js
@@ -4,14 +4,31 @@ let products = [
     { id: 2, price: 60000, title: "Black Monastery" },
 ];
 
-let productCard = `<div class="col-sm-4">
+function appendProductCard(products) {
+    let productCard = `
+                <div class="col-sm-4">
                     <img src="https://via.placeholder.com/600" class="w-100" />
                     <h5 class="item-title"></h5>
                     <p class="item-price"></p>
                 </div>`;
 
-products.forEach(item => {
-    $(".card-row").append(productCard);
-    $(".item-title").eq(item.id).html(item.title);
-    $(".item-price").eq(item.id).html("가격: " + item.price);
-});
+    products.forEach(item => {
+        $(".card-row").append(productCard);
+        $(".item-title").eq(item.id).html(item.title);
+        $(".item-price").eq(item.id).html("가격: " + item.price);
+    });
+}
+
+appendProductCard(products);
+
+let showMoreCnt = 0;
+
+$("#show-more-btn").on("click", () => {
+    $.get(`https://codingapple1.github.io/js/more${++showMoreCnt}.json`)
+    .done((data) => {
+        appendProductCard(data);
+    })
+    .fail(() => {
+        console.log("Fail!!");
+    });
+})
