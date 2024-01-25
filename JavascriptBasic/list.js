018ab28088bca08ef3b729e9c8ce526e42682ab2
ljh37694 <ljh37694@naver.com>
2024-01-25 19:13:41 +0900

    가격순 정렬

--- JavascriptBasic/list.js
+++ JavascriptBasic/list.js
@@ -1,23 +1,27 @@
+function appendProductCard(products) {
+    products.forEach((item, idx) => {
+        let productCard = `
+        <div class="col-sm-4">
+            <img src="https://via.placeholder.com/600" class="w-100" />
+            <h5 class="item-title">${item.title}</h5>
+            <p class="item-price">가격: ${item.price}</p>
+        </div>`;
+
+        $(".card-row").append(productCard);
+    });
+}
+
 let products = [
     { id: 0, price: 70000, title: "Blossom Dress" },
     { id: 1, price: 50000, title: "Springfield Shirt" },
     { id: 2, price: 60000, title: "Black Monastery" },
 ];
 
-function appendProductCard(products) {
-    let productCard = `
-                <div class="col-sm-4">
-                    <img src="https://via.placeholder.com/600" class="w-100" />
-                    <h5 class="item-title"></h5>
-                    <p class="item-price"></p>
-                </div>`;
+products.sort((a, b) => {
+    return a.price - b.price;
+});
 
-    products.forEach(item => {
-        $(".card-row").append(productCard);
-        $(".item-title").eq(item.id).html(item.title);
-        $(".item-price").eq(item.id).html("가격: " + item.price);
-    });
-}
+console.log(products);
 
 appendProductCard(products);
 
