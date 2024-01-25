c3e5afadc2f95033ad36c57d3aaaaa5314c88c90
ljh37694 <ljh37694@naver.com>
2024-01-25 18:36:48 +0900

    변수로 html 요소 넣기

--- JavascriptBasic/list.js
+++ JavascriptBasic/list.js
@@ -4,7 +4,14 @@ let products = [
     { id: 2, price: 60000, title: "Black Monastery" },
 ];
 
+let productCard = `<div class="col-sm-4">
+                    <img src="https://via.placeholder.com/600" class="w-100" />
+                    <h5 class="item-title"></h5>
+                    <p class="item-price"></p>
+                </div>`;
+
 products.forEach(item => {
+    $(".card-row").append(productCard);
     $(".item-title").eq(item.id).html(item.title);
     $(".item-price").eq(item.id).html("가격: " + item.price);
 });
