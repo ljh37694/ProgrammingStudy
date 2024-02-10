323e477284963d3f2796956856fb3e3eb71debb8
ljh37694 <ljh37694@naver.com>
2024-02-10 14:58:11 +0900

    useState, onClick 써보기

--- ReactPractice/blog/src/App.js
+++ ReactPractice/blog/src/App.js
@@ -1,8 +1,17 @@
-import logo from './logo.svg';
+/* eslint-disable */
+
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   let postTitle = "메가 커피";
+  let arr = ["남자 코트 추천", "강남 우동 맛집", "파이썬 독학"];
+  let [posts, setPosts] = useState(arr);
+  let [likeCount, setLikeCount] = useState(0);
+
+  function likeOnClick() {
+    setLikeCount(likeCount + 1);
+  }
 
   return (
     <div className="App">
@@ -13,7 +22,24 @@ function App() {
           블로그임
         </h4>
       </div>
-      <h4 style={{fontSize: "30px"}}>{postTitle}</h4>
+      <div className="item">
+        <h4 style={{fontSize: "30px"}}>{posts[0]}</h4>
+        <p>2024년 2월 10일 발행</p>
+        <button onClick={likeOnClick}>👍</button>
+        <span>{likeCount}</span>
+      </div>
+      <div className="item">
+        <h4 style={{fontSize: "30px"}}>{posts[1]}</h4>
+        <p>2024년 2월 10일 발행</p>
+      </div>
+      <div className="item">
+        <h4 style={{fontSize: "30px"}}>{posts[2]}</h4>
+        <p>2024년 2월 10일 발행</p>
+      </div>
+      <button onClick={() => {
+        arr[0] = "여자 코트 추천";
+        setPosts(arr);
+      }}>click!</button>
     </div>
   );
 }
