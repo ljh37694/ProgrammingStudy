0eb402a2b71630bcedd8bcf35e36e665c0aed4c6
ljh37694 <ljh37694@naver.com>
2024-02-12 01:33:23 +0900

    props 이용해서 modal 만들기

--- ReactPractice/blog/src/App.js
+++ ReactPractice/blog/src/App.js
@@ -3,21 +3,28 @@
 import './App.css';
 import { useState } from 'react';
 
+function Modal(props) {
+  return (
+    <div className="modal">
+      <h4>{props.title[0]}</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+      <button onClick={props.clickChangeBtn}>수정</button>
+    </div>
+  );
+}
+
 function App() {
   let postTitle = "메가 커피";
   let arr = ["남자 코트 추천", "강남 우동 맛집", "파이썬 독학"];
   let [posts, setPosts] = useState(arr);
   let [likeCounts, setLikeCount] = useState([0, 0, 0]);
   let [modalVisible, setModalVisible] = useState(false);
-  
-  function Modal(props) {
-    return (
-      <div className="modal">
-        <h4>제목</h4>
-        <p>날짜</p>
-        <p>상세내용</p>
-      </div>
-    );
+  let clickChangeBtn = () => {
+    let copyArr = [...posts];
+    copyArr[0] = "여자 코트 추천";
+
+    setPosts(copyArr);
   }
 
   return (
@@ -46,20 +53,13 @@ function App() {
 
       <button onClick={() => {
         let copyArr = [...posts];
-        copyArr[0] = "여자 코트 추천";
-
-        setPosts(copyArr);
-      }}>수정</button>
-
-      <button onClick={() => {
-        let copyArr = [...posts];
         copyArr.sort();
 
         setPosts(copyArr);
       }}>정렬</button>
 
       {
-        modalVisible === true ? <Modal/> : null
+        modalVisible === true ? <Modal title={posts} clickChangeBtn={clickChangeBtn} /> : null
       }
     </div>
   );
