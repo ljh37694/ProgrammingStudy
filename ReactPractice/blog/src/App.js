6b9b58b4291497a9357ad614cc6936621dd067d0
ljh37694 <ljh37694@naver.com>
2024-02-11 22:27:16 +0900

    map 함수를 이용해서 비슷한 html 반복 출력 및 모달창 제목 누르면 뜨도록 함

--- ReactPractice/blog/src/App.js
+++ ReactPractice/blog/src/App.js
@@ -7,10 +7,17 @@ function App() {
   let postTitle = "메가 커피";
   let arr = ["남자 코트 추천", "강남 우동 맛집", "파이썬 독학"];
   let [posts, setPosts] = useState(arr);
-  let [likeCount, setLikeCount] = useState(0);
-
-  function likeOnClick() {
-    setLikeCount(likeCount + 1);
+  let [likeCounts, setLikeCount] = useState([0, 0, 0]);
+  let [modalVisible, setModalVisible] = useState(false);
+  
+  function Modal(props) {
+    return (
+      <div className="modal">
+        <h4>제목</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
+    );
   }
 
   return (
@@ -22,32 +29,38 @@ function App() {
           블로그임
         </h4>
       </div>
-      <div className="item">
-        <h4 style={{fontSize: "30px"}}>{posts[0]}</h4>
-        <p>2024년 2월 10일 발행</p>
-        <button onClick={likeOnClick}>👍</button>
-        <span>{likeCount}</span>
-      </div>
-      <div className="item">
-        <h4 style={{fontSize: "30px"}}>{posts[1]}</h4>
-        <p>2024년 2월 10일 발행</p>
-      </div>
-      <div className="item">
-        <h4 style={{fontSize: "30px"}}>{posts[2]}</h4>
-        <p>2024년 2월 10일 발행</p>
-      </div>
+      { posts.map((item, idx) => {
+        return (
+          <div className="item" key={idx}>
+            <h4 onClick={() => setModalVisible(!modalVisible)} style={{fontSize: "30px"}}>{item}</h4>
+            <p>2024년 2월 10일 발행</p>
+            <button onClick={() => {
+                  let copyLikeCounts = [...likeCounts];
+                  copyLikeCounts[idx]++;
+                  setLikeCount(copyLikeCounts);
+            }}>👍</button>
+            <span>{likeCounts[idx]}</span>
+          </div>
+        )
+      }) }
+
       <button onClick={() => {
         let copyArr = [...posts];
         copyArr[0] = "여자 코트 추천";
 
         setPosts(copyArr);
       }}>수정</button>
+
       <button onClick={() => {
         let copyArr = [...posts];
         copyArr.sort();
 
         setPosts(copyArr);
       }}>정렬</button>
+
+      {
+        modalVisible === true ? <Modal/> : null
+      }
     </div>
   );
 }
