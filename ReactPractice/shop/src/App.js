4b702b10d379cc21b62c7d4b46b92341d66c5cab
ljh37694 <ljh37694@naver.com>
2024-02-12 16:49:25 +0900

    상품 카드 만들기

--- ReactPractice/shop/src/App.js
+++ ReactPractice/shop/src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import data from "./data.js";
+import { useState } from "react";
+
+function ShoesItems(props) {
+    let itemData = props.data;
+
+    return (
+        <div>
+            <div className="row">
+                {itemData.map((item, idx) => {
+                    return (
+                        <div className="col-md-4" key={idx}>
+                            <img
+                                src={`https://codingapple1.github.io/shop/shoes${idx + 1}.jpg`}
+                                width="80%"
+                            />
+                            <h4>{item.title}</h4>
+                            <p>{item.content}</p>
+                            <p>{item.price}원</p>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let [itemData, setItemData] = useState(data);
+    return (
+        <div className="App">
+            <Navbar expand="lg" className="bg-body-tertiary">
+                <Container className="ms-0">
+                    <Navbar.Brand href="#home">Shoeshop</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#home">Cart</Nav.Link>
+                            <Nav.Link href="#link">Link</Nav.Link>
+                            <NavDropdown title="더보기" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">
+                                    Action
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">
+                                    Another action
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">
+                                    Something
+                                </NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">
+                                    Separated link
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            <div className="main-image w-100 m-0"></div>
+            <ShoesItems data={data}/>
+        </div>
+    );
 }
 
 export default App;
