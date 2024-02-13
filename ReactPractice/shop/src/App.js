42efb04a5605daf7011748e90c11dc3cc7866d65
ljh37694 <ljh37694@naver.com>
2024-02-13 13:44:44 +0900

    nested routes 실습

--- ReactPractice/shop/src/App.js
+++ ReactPractice/shop/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import data from "./data.js";
 import { useState } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Outlet, Route, Routes } from "react-router-dom";
 import MainPage from "./component/MainPage.js";
 import DetailPage from "./component/DetailPage.jsx";
 import MainNavbar from "./component/MainNavbar.jsx";
@@ -13,8 +13,23 @@ function App() {
         <div className="App">
             <MainNavbar />
             <Routes>
-                <Route path="/detail" element={ <DetailPage /> } />
-                <Route path="/" element={ <MainPage itemData={itemData}/> } />
+                <Route path="/detail" element={<DetailPage />} />
+                <Route path="/" element={<MainPage itemData={itemData} />} />
+                <Route
+                    path="/event"
+                    element={
+                        <>
+                            <h3>오늘의 이벤트</h3>
+                            <Outlet />
+                        </>
+                    }
+                >
+                    <Route
+                        path="one"
+                        element={<p>첫 주문시 양배추즙 증정</p>}
+                    />
+                    <Route path="two" element={<p>생일 쿠폰 받기</p>} />
+                </Route>
             </Routes>
         </div>
     );
