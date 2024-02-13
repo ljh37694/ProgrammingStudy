1107e6ec470fd122f11689c997daafbe22a41292
ljh37694 <ljh37694@naver.com>
2024-02-14 02:29:37 +0900

    useEffect 실습

--- ReactPractice/shop/src/pages/DetailPage.jsx
+++ ReactPractice/shop/src/pages/DetailPage.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 function DetailPage(props) {
@@ -7,8 +8,15 @@ function DetailPage(props) {
         return item.id == id;
     });
 
+    useEffect(() => {
+        setTimeout(() => {
+            document.getElementsByClassName("alert")[0].style.visibility= "hidden";
+        }, 2000);
+    });
+
     return (
         <div className="container">
+            <div className="alert alert-warning">2초 이내 구매시 할인</div>
             <div className="row">
                 <div className="col-md-6">
                     <img
