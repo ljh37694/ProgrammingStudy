e38dc7aa2c9f7e0e81251258d09cde884cc07b37
ljh37694 <ljh37694@naver.com>
2024-02-17 22:28:44 +0900

    최근 본 상품 ui 만들기

--- ReactPractice/shop/src/pages/MainPage.jsx
+++ ReactPractice/shop/src/pages/MainPage.jsx
@@ -1,15 +1,45 @@
 import axios from "axios";
 import ShoesItems from "../components/ShoesItems";
 import { useState } from "react";
-import { useSelector } from "react-redux";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+let WatchedItems = styled.div`
+    position: fixed;
+    left: 80%;
+    top: 100px;
+    width: 100px;
+    height: 200px;
+    background-color: #fff;
+    border: 1px solid black;
+    overflow-y: scroll;
+
+    &::-webkit-scrollbar {
+        display: none;
+    }
+`;
 
 function MainPage(props) {
     let { itemData, setItemData, count, setCount } = props;
     let [loading, setLoading] = useState(false);
+    let watched = JSON.parse(localStorage.getItem("watchedItems"));
 
     return (
         <>
             <div className="main-image w-100 m-0"></div>
+            <WatchedItems>
+                <Link to="/cart" style={{textDecorationLine: "none"}}>
+                    <h6 className="p-2" style={{backgroundColor: "grey", color: "#fff"}}>CART</h6>
+                </Link>
+                <p style={{fontSize: "12px"}}>최근 본 상품</p>
+                { watched.map((id) => {
+                    return (
+                        <p key={id}>{ itemData.find((item) => {
+                            return item.id == id;
+                        }).title }</p>
+                    );
+                }) }
+            </WatchedItems>
             <ShoesItems data={itemData} />
             <button
                 className="btn btn-danger me-3"
