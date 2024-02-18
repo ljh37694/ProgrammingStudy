a6802d4ecc5293fe5fa8f8deb9b72a83d4e8676e
ljh37694 <ljh37694@naver.com>
2024-02-18 19:46:01 +0900

    실시간 처리에 용이한 react-query 설치

--- ReactPractice/shop/src/index.js
+++ ReactPractice/shop/src/index.js
@@ -6,8 +6,10 @@ import reportWebVitals from "./reportWebVitals";
 import { createBrowserRouter, RouterProvider, BrowserRouter } from "react-router-dom";
 import store from './store';
 import { Provider } from 'react-redux';
+import { QueryClient, QueryClientProvider } from "react-query"; }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const queryClient = new QueryClient();
 const router = createBrowserRouter([
     {
         path: "/",
@@ -16,13 +18,15 @@ const router = createBrowserRouter([
 ]);
 
 root.render(
-    <Provider store={store}>
-        <React.StrictMode>
-            <BrowserRouter>
-                <App></App>
-            </BrowserRouter>
-        </React.StrictMode>
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+        <Provider store={store}>
+            <React.StrictMode>
+                <BrowserRouter>
+                    <App></App>
+                </BrowserRouter>
+            </React.StrictMode>
+        </Provider>
+    </QueryClientProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
