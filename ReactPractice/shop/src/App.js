7e296c059c64bbcba517727bc47a55cdabd8dc9e
ljh37694 <ljh37694@naver.com>
2024-02-18 21:51:53 +0900

    lazy, Suspense 사용해서 성능 향상

--- ReactPractice/shop/src/App.js
+++ ReactPractice/shop/src/App.js
@@ -1,15 +1,16 @@
 import "./App.css";
 import data from "./data.js";
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect, lazy, Suspense } from "react";
 import { Outlet, Route, Routes } from "react-router-dom";
-import MainPage from "./pages/MainPage.jsx";
-import DetailPage from "./pages/DetailPage.jsx";
 import MainNavbar from "./components/MainNavbar.jsx";
-import CartPage from "./pages/CartPage.jsx";
+import MainPage from "./pages/MainPage.jsx";
 import { useQuery } from "react-query";
 import axios from "axios";
 
 export let Context1 = createContext();
+
+const DetailPage = lazy(() => import("./pages/DetailPage.jsx"));
+const CartPage = lazy(() => import("./pages/CartPage.jsx"));
 
 function App() {
     let [itemData, setItemData] = useState(data);
@@ -33,43 +34,46 @@ function App() {
             {result.error && "에러남"}
             {result.data && result.data.name}
             <MainNavbar />
-            <Routes>
-                <Route
-                    path="/"
-                    element={
-                        <MainPage
-                            itemData={itemData}
-                            setItemData={setItemData}
-                            count={count}
-                            setCount={setCount}
-                        />
-                    }
-                />
-                <Route
-                    path="/detail/:id"
-                    element={
-                        <Context1.Provider value={stock}>
-                            <DetailPage itemData={itemData} />
-                        </Context1.Provider>
-                    }
-                />
-                <Route
-                    path="/event"
-                    element={
-                        <>
-                            <h3>오늘의 이벤트</h3>
-                            <Outlet />
-                        </>
-                    }
-                >
+
+            <Suspense fallback={<div>로딩하는 중</div>}>
+                <Routes>
                     <Route
-                        path="one"
-                        element={<p>첫 주문시 양배추즙 증정</p>}
+                        path="/"
+                        element={
+                            <MainPage
+                                itemData={itemData}
+                                setItemData={setItemData}
+                                count={count}
+                                setCount={setCount}
+                            />
+                        }
                     />
-                    <Route path="two" element={<p>생일 쿠폰 받기</p>} />
-                </Route>
-                <Route path="/cart" element={<CartPage />} />
-            </Routes>
+                    <Route
+                        path="/detail/:id"
+                        element={
+                            <Context1.Provider value={stock}>
+                                <DetailPage itemData={itemData} />
+                            </Context1.Provider>
+                        }
+                    />
+                    <Route
+                        path="/event"
+                        element={
+                            <>
+                                <h3>오늘의 이벤트</h3>
+                                <Outlet />
+                            </>
+                        }
+                    >
+                        <Route
+                            path="one"
+                            element={<p>첫 주문시 양배추즙 증정</p>}
+                        />
+                        <Route path="two" element={<p>생일 쿠폰 받기</p>} />
+                    </Route>
+                    <Route path="/cart" element={<CartPage />} />
+                </Routes>
+            </Suspense>
         </div>
     );
 }
