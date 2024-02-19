c9dabea9a4f02692d449e67d074fb51b2e180337
ljh37694 <ljh37694@naver.com>
2024-02-19 13:31:01 +0900

    css 적용 실습

--- NodeJsAndMongoDB/server.js
+++ NodeJsAndMongoDB/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static(__dirname + "/public"));
+
 app.listen(1234, () => {
     console.log("http://localhost:1234/ 서버 실행중");
 });
