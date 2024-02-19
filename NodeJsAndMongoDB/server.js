d25a0827c1f37d4fce75185b3a6b3d40299c20df
ljh37694 <ljh37694@naver.com>
2024-02-19 13:16:45 +0900

    웹페이지 전송(Routing)

--- NodeJsAndMongoDB/server.js
+++ NodeJsAndMongoDB/server.js
@@ -2,9 +2,18 @@ const express = require("express");
 const app = express();
 
 app.listen(1234, () => {
-    console.log("서버 실행중");
+    console.log("http://localhost:1234/ 서버 실행중");
 });
 
 app.get("/", (req, res) => {
-    res.send("Hello");
+    // res.send("Hello");
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/news", (req, res) => {
+    res.send("오늘 비옴");
+});
+
+app.get("/about", (req, res) => {
+    res.sendFile(__dirname + "/about.html");
 });
