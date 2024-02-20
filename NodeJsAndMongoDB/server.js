5fe3ed6f2c7990b50fb97e5416db314bfff0bcbf
ljh37694 <ljh37694@naver.com>
2024-02-20 20:54:22 +0900

    ejs를 이용해서 server side rendering 실습

--- NodeJsAndMongoDB/server.js
+++ NodeJsAndMongoDB/server.js
@@ -1,23 +1,27 @@
 const express = require("express");
 const app = express();
+let serverTime = new Date();
 
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 
-const { MongoClient } = require('mongodb')
+const { MongoClient } = require("mongodb");
 
-let db
-const url = "mongodb+srv://ljh37694:hi37694*@forum.6p5dx3j.mongodb.net/?retryWrites=true&w=majority";
-new MongoClient(url).connect().then((client)=>{
-  console.log('DB연결성공');
-  db = client.db('Forum');
+let db;
+const url ="mongodb+srv://ljh37694:hi37694*@forum.6p5dx3j.mongodb.net/?retryWrites=true&w=majority";
+new MongoClient(url)
+    .connect()
+    .then((client) => {
+        console.log("DB연결성공");
+        db = client.db("Forum");
 
-  app.listen(1234, () => {
-    console.log("http://localhost:1234/ 서버 실행중");
-});
-}).catch((err)=>{
-  console.log(err);
-});
+        app.listen(1234, () => {
+            console.log("http://localhost:1234/ 서버 실행중");
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 app.get("/", (req, res) => {
     // res.send("Hello");
@@ -34,7 +38,11 @@ app.get("/about", (req, res) => {
 });
 
 app.get("/list", async (req, res) => {
-  let result = await db.collection("Post").find().toArray();
-  console.log(result);
-  res.send("list");
+    let result = await db.collection("Post").find().toArray();
+    console.log(result);
+    res.render("posts.ejs", { data: result });
+});
+
+app.get("/time", (req, res) => {
+    res.render("time.ejs", { time : serverTime });
 });
