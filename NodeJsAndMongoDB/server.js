e75ed41cb652b5802957c359260958ca0cc0f894
ljh37694 <ljh37694@naver.com>
2024-02-23 13:18:59 +0900

    passport 설치

--- NodeJsAndMongoDB/server.js
+++ NodeJsAndMongoDB/server.js
@@ -2,18 +2,46 @@ const express = require("express");
 const app = express();
 let serverTime = new Date();
 const methodOverride = require("method-override");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
 
+app.use(passport.initialize());
+app.use(
+    session({
+        secret: "암호화에 쓸 비번",
+        resave: false,
+        saveUninitialized: false,
+    })
+);
+app.use(passport.session());
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 app.use(express.json());
-app.use(express.urlencoded({ extended : true }));
-app.use(methodOverride('_method')) ;
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
+passport.use(
+    new LocalStrategy(async (입력한아이디, 입력한비번, cb) => {
+        let result = await db
+            .collection("user")
+            .findOne({ username: 입력한아이디 });
+        if (!result) {
+            return cb(null, false, { message: "아이디 DB에 없음" });
+        }
+        if (result.password == 입력한비번) {
+            return cb(null, result);
+        } else {
+            return cb(null, false, { message: "비번불일치" });
+        }
+    })
+);
 
 const { MongoClient, ObjectId } = require("mongodb");
 
 let db;
-const url ="mongodb+srv://ljh37694:hi37694*@forum.6p5dx3j.mongodb.net/?retryWrites=true&w=majority";
+const url =
+    "mongodb+srv://ljh37694:hi37694*@forum.6p5dx3j.mongodb.net/?retryWrites=true&w=majority";
 new MongoClient(url)
     .connect()
     .then((client) => {
@@ -32,7 +60,7 @@ app.get("/", async (req, res) => {
     let result = await db.collection("post").find().toArray();
 
     // res.send("Hello");
-    res.render("posts.ejs", { data : result });
+    res.render("posts.ejs", { data: result });
 });
 
 app.get("/news", (req, res) => {
@@ -47,11 +75,11 @@ app.get("/about", (req, res) => {
 app.get("/list", async (req, res) => {
     let result = await db.collection("post").find().toArray();
     console.log(result);
-    res.render("posts.ejs", { data : result });
+    res.render("posts.ejs", { data: result });
 });
 
 app.get("/time", (req, res) => {
-    res.render("time.ejs", { time : serverTime });
+    res.render("time.ejs", { time: serverTime });
 });
 
 app.get("/write", (req, res) => {
@@ -66,10 +94,10 @@ app.post("/add", async (req, res) => {
             await db.collection("post").insertOne(req.body, (err, result) => {
                 console.log("저장 완료");
             });
-        
+
             res.redirect("/list");
         }
-    } catch(e) {
+    } catch (e) {
         console.log(e);
         res.status(500).send("서버 에러!!!");
     }
@@ -77,28 +105,32 @@ app.post("/add", async (req, res) => {
 
 app.get("/detail/:_id", async (req, res) => {
     try {
-        const result = await db.collection("post").findOne({_id : new ObjectId(req.params._id)});
+        const result = await db
+            .collection("post")
+            .findOne({ _id: new ObjectId(req.params._id) });
 
         if (result == null) {
             res.status(400).send("그런 글 없음");
         }
 
-        res.render("detail.ejs", { post : result });
-    } catch(e) {
+        res.render("detail.ejs", { post: result });
+    } catch (e) {
         res.send("이거 아님");
     }
 });
 
 app.get("/edit/:id", async (req, res) => {
     try {
-        const result = await db.collection("post").findOne({_id : new ObjectId(req.params.id)});
+        const result = await db
+            .collection("post")
+            .findOne({ _id: new ObjectId(req.params.id) });
 
         if (result == null) {
             res.status(400).send("그런 글 없음");
         }
 
-        res.render("edit.ejs", { post : result });
-    } catch(e) {
+        res.render("edit.ejs", { post: result });
+    } catch (e) {
         res.send("이거 아님");
     }
 });
@@ -108,24 +140,53 @@ app.put("/edit-post/:id", async (req, res) => {
         if (req.body.title == "" || req.body.content == "") {
             alert("빈 칸이 있음");
         } else {
-            await db.collection("post").updateOne({ _id : new ObjectId(req.params.id)}, { $set : {title : req.body.title, content : req.body.content} });
-        
+            await db.collection("post").updateOne(
+                { _id: new ObjectId(req.params.id) },
+                {
+                    $set: {
+                        title: req.body.title,
+                        content: req.body.content,
+                    },
+                }
+            );
+
             res.redirect("/list");
         }
-    } catch(e) {
+    } catch (e) {
         console.log(e);
         res.status(500).send("서버 에러!!!");
     }
 });
 
 app.delete("/delete-post", async (req, res) => {
-    await db.collection("post").deleteOne({ _id : new ObjectId(req.query.id) });
+    await db.collection("post").deleteOne({ _id: new ObjectId(req.query.id) });
 });
 
 app.get("/list/:number", async (req, res) => {
     let num = parseInt(req.params.number);
     const LIM = 5;
-    let result = await db.collection("post").find().skip(num * LIM).limit(LIM).toArray();
-    
-    res.render("posts.ejs", { data : result });
+    let result = await db
+        .collection("post")
+        .find()
+        .skip(num * LIM)
+        .limit(LIM)
+        .toArray();
+
+    res.render("posts.ejs", { data: result });
+});
+
+// 로그인
+app.get("/login", (요청, 응답) => {
+    응답.render("login.ejs");
+});
+
+app.post("/login", async (요청, 응답, next) => {
+    passport.authenticate("local", (error, user, info) => {
+        if (error) return 응답.status(500).json(error);
+        if (!user) return 응답.status(401).json(info.message);
+        요청.logIn(user, (err) => {
+            if (err) return next(err);
+            응답.redirect("/");
+        });
+    })(요청, 응답, next);
 });
