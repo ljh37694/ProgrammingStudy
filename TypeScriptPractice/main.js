cdb43441078f4725529ae4f1aa1996d8c967f35a
i_junghoon <ljh37694@naver.com>
2024-04-03 13:19:15 +0900

    init

---
+++ TypeScriptPractice/main.js
@@ -0,0 +1,6 @@
+function PrimitiveType() {
+    var name = "Lee";
+    var age = 23;
+    var birthLocation = "경기도 성남시";
+    console.log(name, age, birthLocation);
+}
---
+++ TypeScriptPractice/main.ts
@@ -0,0 +1,7 @@
+function PrimitiveType() {
+    let name : string = "Lee";
+    let age : number = 23;
+    let birthLocation : string = "경기도 성남시"
+
+    console.log(name, age, birthLocation);
+}
