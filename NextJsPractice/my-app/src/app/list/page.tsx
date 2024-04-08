8510950b6cffa452b6ee639605a27c6287113d82
i_junghoon <ljh37694@naver.com>
2024-04-08 13:13:08 +0900

    list 페이지 만들기

---
+++ NextJsPractice/my-app/src/app/list/page.tsx
@@ -0,0 +1,13 @@
+export default function List(): JSX.Element {
+    return (
+        <div className="list-box">
+            <h2 className="main-title">Products</h2>
+            <div className="food-card">
+                <h4>상품명1 $40</h4>
+            </div>
+            <div className="food-card">
+                <h4>상품명2 $140</h4>
+            </div>
+        </div>
+    );
+}
