cfaaebc84ec22b12e7b42bde0b8ca8dac91f65c0
i_junghoon <ljh37694@naver.com>
2024-04-09 14:05:17 +0900

    props 실습

--- NextJsPractice/my-app/src/app/cart/page.tsx
+++ NextJsPractice/my-app/src/app/cart/page.tsx
@@ -1,17 +1,24 @@
 export default function Cart(): JSX.Element {
+    const products: string[] = ["Tomatoes", "Pasta", "Coconut"];
+
     return (
         <div>
             <h4 className="main-title">Cart</h4>
-            <CartItem />
-            <CartItem />
+            {products.map((item: string) => {
+                return (
+                    <CartItem name={item} />
+                );
+            })}
         </div>
     );
 }
 
 function CartItem(props: any) {
+    const { name } = props;
+
     return (
         <div className="cart-item">
-            <p>상품명</p>
+            <p>{name}</p>
             <p>$40</p>
             <p>1개</p>
         </div>
--- NextJsPractice/my-app/src/app/page.tsx
+++ NextJsPractice/my-app/src/app/page.tsx
@@ -1,10 +1,10 @@
 export default function Home(): JSX.Element {
-  const name: string = "Lee";
+    const name: string = "Lee";
 
-  return (
-    <main className="main-page">
-      <h3 className="main-title">Apple Fresh</h3>
-      <p className="sub-title">by dev {name}</p>
-    </main>
-  );
+    return (
+        <main className="main-page">
+            <h3 className="main-title">Apple Fresh</h3>
+            <p className="sub-title">by dev {name}</p>
+        </main>
+    );
 }
