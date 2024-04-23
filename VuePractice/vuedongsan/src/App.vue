4846e10f95489303d57776fa57ac4ae4399ad8a8
i_junghoon <ljh37694@naver.com>
2024-04-23 13:15:57 +0900

    vue 데이더 마이닝 실습

--- VuePractice/vuedongsan/src/App.vue
+++ VuePractice/vuedongsan/src/App.vue
@@ -1,15 +1,31 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h4 :style="redColor">{{ products[0] }}</h4>
+    <p>{{ price1 }}</p>
+  </div>
+    <div>
+    <h4 :style="redColor">{{ products[1] }}</h4>
+    <p>{{ price1 }}</p>
+  </div>
+    <div>
+    <h4 :style="redColor">{{ products[2] }}</h4>
+    <p>{{ price1 }}</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      price1: 60,
+      redColor: "color: red",
+      products: ["역삼동 원룸", "천호동 원룸", "마포구 원룸"],
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
