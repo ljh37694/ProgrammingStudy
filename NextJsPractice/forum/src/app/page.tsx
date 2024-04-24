ffdbd661dce42288cdcf76d04d780b3fb64cdc2f
i_junghoon <ljh37694@naver.com>
2024-04-24 22:02:55 +0900

    mongodb 설치

--- NextJsPractice/forum/src/app/page.tsx
+++ NextJsPractice/forum/src/app/page.tsx
@@ -1,3 +1,13 @@
-export default function Home() {
-    return <main></main>;
+import { connectDB } from "@/database";
+
+export default async function Home() {
+    const client = await connectDB;
+    const db = client.db("NextForum");
+    let data: any = await db.collection("posts").find().toArray();
+
+    return (
+        <main>
+            {data}
+        </main>
+    );
 }
