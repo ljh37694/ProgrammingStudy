2b752cedaafa1558affe2045f4a1c289b6159934
i_junghoon <ljh37694@naver.com>
2024-04-26 21:21:56 +0900

    vue Transition 태그 사용해서 애니메이션 적용 실습

--- VuePractice/vuedongsan/src/components/Modal.vue
+++ VuePractice/vuedongsan/src/components/Modal.vue
@@ -1,14 +1,17 @@
 <template>
-  <div class="black-bg" v-if="activeModal == true">
-    <div class="white-bg">
-      <h4>{{ productsData[clickedIdx].title }}</h4>
-      <p>{{ productsData[clickedIdx].content }}</p>
-      <input v-model="month">
-      <p>{{ month }}개월: {{ productsData[clickedIdx].price * month }}</p>
-      
-      <button @click="$emit('closeModal')">닫기</button>
+  <Transition name="fade">
+    <div class="black-bg" v-if="activeModal == true">
+      <div class="white-bg">
+        <h4>{{ productsData[clickedIdx].title }}</h4>
+        <p>{{ productsData[clickedIdx].content }}</p>
+        <input v-model="month">
+        <p>{{ month }}개월: {{ productsData[clickedIdx].price * month }}</p>
+
+        <button @click="$emit('closeModal')">닫기</button>
+      </div>
     </div>
-  </div>
+  </Transition>
+
 </template>
 
 <script>
@@ -35,4 +38,24 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+  .fade-enter-from {
+    opacity: 0;
+  }
+  .fade-enter-active {
+    transition: all 0.5s;
+  }
+  .fade-enter-to {
+    opacity: 1;
+  }
+
+  .fade-leave-from {
+    opacity: 1;
+  }
+  .fade-leave-active {
+    transition: all 1s;
+  }
+  .fade-leave-to {
+    opacity: 0;
+  }
+</style>
