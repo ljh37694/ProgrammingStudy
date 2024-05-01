0dc3cac98e4b1c2a84cf35915e52bbf42da4d6af
i_junghoon <ljh37694@naver.com>
2024-05-01 13:36:08 +0900

    vue axios 실습

--- VuePractice/vuestagram/src/App.vue
+++ VuePractice/vuestagram/src/App.vue
@@ -1,18 +1,30 @@
 <template>
   <main id="vuestagram">
     <Container :postData="postData" />
+    <button @click="more">더보기</button>
   </main>
 </template>
 
 <script>
 import Container from './components/Container.vue';
 import postData from './assets/postData';
+import axios from 'axios';
 
 export default {
   name: 'App',
   data() {
     return {
       postData: postData,
+      moreCount: 0,
+    }
+  },
+  methods: {
+    more() {
+      axios.get(`https://codingapple1.github.io/vue/more${this.moreCount++}.json`)
+      .then(res => {
+        this.postData = [...this.postData, res.data];
+      })
+      .catch(e => console.log(e));
     }
   },
   components: {
